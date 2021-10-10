a1907c27304b5b6c1535e02dd7cf652091285812
CoderMahabub <codermahabub@gmail.com>
2021-10-10 17:20:16 +0600

    Toggle LogIn & Registration Page

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./Login/Login";
 import Register from "./Login/Register";
 import firebaseInitialize from "./Firebase/Firebase.init";
+import { useState } from "react";
 
 
 firebaseInitialize();
 
 function App() {
+  const [toggle, setToggle] = useState(false);
   return (
     <div className="App container ">
       <div className="row">
         <div className="login-area col-md-6">
-          <Login></Login>
-          {/* <Register></Register> */}
+          {toggle ? <Login></Login> : <Register></Register>}
 
-          <p className="text-primary ">
+          {toggle ? <p onClick={() => setToggle(false)} className="text-primary ">
             are you new please register
-          </p>
-          <p className="text-primary ">
-            already have an account ?
-          </p>
+          </p> :
+            <p onClick={() => setToggle(true)} className="text-primary ">
+              already have an account ?
+            </p>}
         </div>
         <div className="col-md-6">
           <div className="img">
--- src/Login/Register.js
+++ src/Login/Register.js
@@ -1,31 +1,76 @@
 import "./Login.css";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import firebaseInitialize from "../Firebase/Firebase.init";
+import { useState } from "react";
+
+firebaseInitialize();
 
 const Register = () => {
+  const auth = getAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState({});
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  }
+  const handlePasswordChange = e => {
+
+    if ((e.target.value.length) < 6) {
+      console.error('Hobe Na Bhai');
+    } else {
+      setPassword(e.target.value);
+    }
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const { email, displayName, photoURL } = result.user;
+        // console.log(user);
+        const userInfo = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(userInfo);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
   return (
     <div>
+      <h1>{user.email}</h1>
       <div className="login-box d-flex align-items-center justify-content-center">
         <div className="login">
           <div className="login-box">
-            <h2 className="text-info">Pease Register</h2>
-            <form>
+            <h2 className="text-info">Please Register</h2>
+            <form onSubmit={handleRegister}>
               <input
+                onBlur={handleEmailChange}
                 className="input-felid"
                 type="email"
                 name="email"
                 placeholder="Enter your Email"
+                required
               />
               <br />
               <input
+                onBlur={handlePasswordChange}
                 className="input-felid"
                 type="password"
                 name="password"
                 placeholder="Enter your Password"
+                required
               />
-              <input
+              <button
                 className="mt-3 w-50 btn btn-success m-auto"
                 type="submit"
                 value="Register"
-              />
+              >Register</button>
             </form>
           </div>
         </div>
